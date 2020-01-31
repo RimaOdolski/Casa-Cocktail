223ddcdf1c2ab4e9556630a04c87dae3c98b6102
Eric Garcia <egarcia1868@gmail.com>
2020-01-31 17:32:41 -0600

    stopping the endless loop.  Now finding why final array won't build

--- script4E.js
+++ script4E.js
@@ -1,4 +1,4 @@
-var ingredientsArr = ["water", "angostura bitters", "bourbon"];
+var ingredientsArr = ["bourbon", "water", "angostura bitters"];
 var currentIngDrnksIDArr = [];
 var canMakeDrinksArr = [];
 // var settings = {
@@ -22,12 +22,12 @@ for (var ingredientsCount = 0; ingredientsCount < ingredientsArr.length; ingredi
   // This will loop through the returned drinks from the current ing and push their IDs to the currentIngDrnksIDArr  MAY NOT NEED CURRENTINGDRNKSIDARR
   $.ajax(settings2).done(function (response2) {
 
-//     console.log(response2);
+    // console.log("response2 = "+response2);
 //   })
 // }
-    // for (var availDrinksCount = 0; availDrinksCount < response2.drinks.length; availDrinksCount++) { BREAKING!!!
+    for (var availDrinksCount = 0; availDrinksCount < response2.drinks.length; availDrinksCount++) {
 
-      console.log(availDrinksCount)
+      // console.log("availDrinksCount = "+availDrinksCount)
       var settings3 = {  
         "url": `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${response2.drinks[availDrinksCount].idDrink}`,
         "method": "GET",
@@ -36,24 +36,25 @@ for (var ingredientsCount = 0; ingredientsCount < ingredientsArr.length; ingredi
       
       $.ajax(settings3).done(function (response3) {
         var haveIngredient;
-        console.log(response3);
-        // for (reqIngCount = 0; reqIngCount < 14 || response3.drinks[0].strIngredient+`${reqIngCount+1}` !== null; reqIngCount++) { BREAKING!!!!!!
-          console.log(response3.drinks[0].strIngredient+`${reqIngCount+1}`);
-          // for (var haveIngCount = 0; haveIngCount < ingredientsArr.length; haveIngCount++) {
-          // var newSource = `strIngredient${reqIngCount}`;
+        // console.log("response3 = "+response3);
+        for (reqIngCount = 0; reqIngCount < 15 && response3.drinks[0].strIngredient+`${reqIngCount+1}` !== null; reqIngCount++) { 
+          // console.log(response3.drinks[0].strIngredient+`${reqIngCount+1}`);
+          for (var haveIngCount = 0; haveIngCount < ingredientsArr.length; haveIngCount++) {
+          var newSource = `strIngredient${reqIngCount}`;
           // console.log("newSource ---->" + newSource)
-          // if (ingredientsArr.contains(response3.drinks[0].newSource)) {
-          //   haveIngredient = true;
-          // } else {
-          //   haveIngredient = false;
-          //   break;
-          // }
+          if (ingredientsArr.includes(response3.drinks[0].newSource)) {
+            haveIngredient = true;
+          } else {
+            haveIngredient = false;
+            break;
+          }
           
         }
         if (haveIngredient === true) {
           canMakeDrinksArr.push(response3.drinks[0].idDrink)
         }
-      });
+      }
+    });
     }     
   });
 };
@@ -65,4 +66,4 @@ for (var ingredientsCount = 0; ingredientsCount < ingredientsArr.length; ingredi
 //   });
 
 // };
-// console.log(canMakeDrinksArr);
+console.log(canMakeDrinksArr);
